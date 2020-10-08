b38bce5f9966b92f70a5b8c5ccb2603f4f76389d
Matteo Nastasi <nastasi@openquake.org>
2020-10-08 20:33:33 +0200

    fix sed typo to extract tar archive name properly

--- packager.sh
+++ packager.sh
@@ -337,7 +337,7 @@ _buildfromsrc_innervm_run () {
         pkg_base="$(echo "$pkg_dsc" | sed 's/.dsc$//g')"
         pkg_changes="${pkg_bass}_source.changes"
         pkg_debarch="${pkg_base}.debian.tar.xz"
-        pkg_orig="$(echo "$pkg_dsc" | sed 's/-.*//g').orig.tar.gz"
+        pkg_orig="$(echo "$pkg_dsc" | sed "s/^${GEM_DEB_PACKAGE}.*-.*//g").orig.tar.gz"
         if [ ! -e "$pkg_dsc" -o ! -e "$pkg_changes" -o ! -e "$pkg_debarch" -o ! -e "$pkg_orig" ]; then
             exit 3
         fi
